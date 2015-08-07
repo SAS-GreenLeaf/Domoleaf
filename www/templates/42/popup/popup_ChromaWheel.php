a9030cf01acadb4119a17dccdd00df84bca23003
Adrien Courtois <adrien.courtois@gmail.com>
2015-08-07 03:43:29 +0200

    added what i was asked for

--- www/templates/42/popup/popup_ChromaWheel.php
+++ www/templates/42/popup/popup_ChromaWheel.php
@@ -3,14 +3,6 @@
 include('header.php');
 
 echo
-<<<<<<< HEAD
 
-=======
-'<div id="colorpicker"></div>'.
-'<form><input type="text" id="color" name="color" value="#123456" disabled="disabled" /></form>'.
-'<script type="text/javascript">'.
-'$("#colorpicker").farbtastic("#color");'.
-'</script>';
->>>>>>> 0291d28... added new templates + added features to upload, delete, update the background image
 
 ?>
