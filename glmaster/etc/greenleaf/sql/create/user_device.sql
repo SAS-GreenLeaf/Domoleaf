1ee494f4fd623900bde4f54a61837ce6b3a59f94
V-Paranoiaque <vr@greenleaf.fr>
2015-08-13 16:35:41 +0200

    Update user_device, for widget's personalization

--- glmaster/etc/greenleaf/sql/create/user_device.sql
+++ glmaster/etc/greenleaf/sql/create/user_device.sql
@@ -1,8 +1,9 @@
-CREATE TABLE IF NOT EXISTS `user_device` (
+CREATE TABLE `user_device` (
   `user_id` int(10) unsigned NOT NULL,
   `room_device_id` int(10) unsigned NOT NULL,
   `device_allowed` tinyint(3) unsigned NOT NULL DEFAULT '0',
   `device_order` int(10) unsigned NOT NULL DEFAULT '0',
+  `device_bgimg` varchar(255) NOT NULL DEFAULT '',
   KEY `user_id` (`user_id`),
   KEY `room_device_id` (`room_device_id`),
   CONSTRAINT `user_device_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE,
