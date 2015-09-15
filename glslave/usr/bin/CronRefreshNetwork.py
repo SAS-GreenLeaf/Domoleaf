444647b6d209a7b51fc407b95d84b62f8da629f7
V-Paranoiaque <vr@greenleaf.fr>
2015-09-15 11:35:01 +0200

    Remove comments

--- glslave/usr/bin/CronRefreshNetwork.py
+++ glslave/usr/bin/CronRefreshNetwork.py
@@ -9,16 +9,17 @@ SLAVE_PREFIX = "SD3"
 
 def send_command(ip, port):
     try:
-        s = socket.create_connection((ip, port)) # faire un systeme avec fichier de conf
+        s = socket.create_connection((ip, port))
         obj = {
             "packet_type": "monitor_ip"
         };
         obj_str = json.JSONEncoder().encode(obj);
-        print("[ CRON REFRESH NETWORK ]: Sending " + obj_str + " to " + ip + ":" + str(port))
+        #print("[ CRON REFRESH NETWORK ]: Sending " + obj_str + " to " + ip + ":" + str(port))
         s.send(obj_str.encode())
         s.close()
     except Exception as e:
-        print("[ CRON REFRESH NETWORK ]: No connection available. Trying again in 15 min")
+        #print("[ CRON REFRESH NETWORK ]: No connection available. Trying again in 15 min")
+        s.close()
 
 if __name__ == "__main__":
     hostname = socket.gethostname()
