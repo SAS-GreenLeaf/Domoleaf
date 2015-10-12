83b64da85b42e573512d529348411171958732fd
V-Paranoiaque <vr@greenleaf.fr>
2015-10-12 11:38:34 +0200

    Default value for configuration

--- glmaster/etc/greenleaf/sql/create/configuration.sql
+++ glmaster/etc/greenleaf/sql/create/configuration.sql
@@ -4,3 +4,19 @@ CREATE TABLE IF NOT EXISTS `configuration` (
   `configuration_description` text,
   PRIMARY KEY (`configuration_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+INSERT IGNORE INTO configuration (`configuration_id`, `configuration_value`, `configuration_description`)
+VALUES
+	(1, '', 'Port public HTTP'),
+	(2, '', 'Port public SSL (HTTPS+SSH)'),
+	(3, '0', 'Force HTTPS'),
+	(4, '', 'Masters version'),
+	(5, '', 'From email'),
+	(6, '', 'From Name'),
+	(7, '', 'SMTP Host'),
+	(8, '0', 'SMTP Secure'),
+	(9, '25', 'SMTP Port'),
+	(10, '', 'SMTP Username'),
+	(11, '', 'SMTP Password'),
+	(12, '', 'Password reset key'),
+	(13, '', 'Last Available Version');
