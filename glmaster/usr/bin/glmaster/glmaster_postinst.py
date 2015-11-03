fc329a8e877a95d2b3b4e2d6fe4da2dadc9f3205
V-Paranoiaque <vr@greenleaf.fr>
2015-11-03 17:26:44 +0100

    Fix db init

--- glmaster/usr/bin/glmaster/glmaster_postinst.py
+++ glmaster/usr/bin/glmaster/glmaster_postinst.py
@@ -54,19 +54,14 @@ def master_conf_initdb():
     query1 = 'DELETE FROM user WHERE User="domoleaf"';
     query2 = 'DELETE FROM db WHERE User="domoleaf"';
     query3 = 'INSERT INTO user (Host, User, Password) VALUES (\'localhost\', \'domoleaf\', PASSWORD(\''+password.hexdigest()+'\'));';
-    query4 = "INSERT INTO db (\`Host\`, \`Db\`, \`User\`, \`Select_priv\`, \`Insert_priv\`, \`Update_priv\`, \`Delete_priv\`, \`Create_priv\`, \`Drop_priv\`, \`Grant_priv\`, \`References_priv\`, \`Index_priv\`, \`Alter_priv\`, \`Create_tmp_table_priv\`, \`Lock_tables_priv\`, \`Create_view_priv\`, \`Show_view_priv\`, \`Create_routine_priv\`, \`Alter_routine_priv\`, \`Execute_priv\`, \`Event_priv\`, \`Trigger_priv\`) VALUES ('localhost','domoleaf','domoleaf','Y','Y','Y','Y','Y','Y','Y','Y','Y','Y','Y','Y','Y','Y','Y','Y','Y','Y','Y');";
+    query4 = 'INSERT INTO db (Host, Db, User, Select_priv, Insert_priv, Update_priv, Delete_priv, Create_priv, Drop_priv, Grant_priv, References_priv, Index_priv, Alter_priv, Create_tmp_table_priv, Lock_tables_priv, Create_view_priv, Show_view_priv, Create_routine_priv, Alter_routine_priv, Execute_priv, Event_priv, Trigger_priv) VALUES ("localhost","domoleaf","domoleaf","Y","Y","Y","Y","Y","Y","Y","Y","Y","Y","Y","Y","Y","Y","Y","Y","Y","Y","Y");';
     query5 = 'FLUSH PRIVILEGES';
     
-    Popen(['mysql', '--defaults-file=/etc/mysql/debian.cnf', 'domoleaf',
-           '-e', query1], stdin=PIPE, stdout=PIPE, stderr=PIPE, bufsize=-1);
-    Popen(['mysql', '--defaults-file=/etc/mysql/debian.cnf', 'domoleaf',
-           '-e', query2], stdin=PIPE, stdout=PIPE, stderr=PIPE, bufsize=-1);
-    Popen(['mysql', '--defaults-file=/etc/mysql/debian.cnf', 'domoleaf',
-           '-e', query3], stdin=PIPE, stdout=PIPE, stderr=PIPE, bufsize=-1);
-    Popen(['mysql', '--defaults-file=/etc/mysql/debian.cnf', 'domoleaf',
-           '-e', query4], stdin=PIPE, stdout=PIPE, stderr=PIPE, bufsize=-1);
-    Popen(['mysql', '--defaults-file=/etc/mysql/debian.cnf', 'domoleaf',
-           '-e', query5], stdin=PIPE, stdout=PIPE, stderr=PIPE, bufsize=-1);
+    Popen(['mysql', '--defaults-file=/etc/mysql/debian.cnf', 'mysql', '-e', query1], stdin=PIPE, stdout=PIPE, stderr=PIPE, bufsize=-1);
+    Popen(['mysql', '--defaults-file=/etc/mysql/debian.cnf', 'mysql', '-e', query2], stdin=PIPE, stdout=PIPE, stderr=PIPE, bufsize=-1);
+    Popen(['mysql', '--defaults-file=/etc/mysql/debian.cnf', 'mysql', '-e', query3], stdin=PIPE, stdout=PIPE, stderr=PIPE, bufsize=-1);
+    Popen(['mysql', '--defaults-file=/etc/mysql/debian.cnf', 'mysql', '-e', query4], stdin=PIPE, stdout=PIPE, stderr=PIPE, bufsize=-1);
+    Popen(['mysql', '--defaults-file=/etc/mysql/debian.cnf', 'mysql', '-e', query5], stdin=PIPE, stdout=PIPE, stderr=PIPE, bufsize=-1);
     
 def master_conf_init():
     file = DaemonConfigParser(SLAVE_CONF_FILE);
