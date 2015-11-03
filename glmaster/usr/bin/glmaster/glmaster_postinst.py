7d6501d78773d960d55b82c33a1c663cfd731470
V-Paranoiaque <vr@greenleaf.fr>
2015-11-03 10:59:02 +0100

    Fix installation scripts

--- glmaster/usr/bin/glmaster/glmaster_postinst.py
+++ glmaster/usr/bin/glmaster/glmaster_postinst.py
@@ -6,14 +6,14 @@ import os;
 import random;
 import string;
 from hashlib import sha1
+from subprocess import *
 import socket;
 sys.path.append("/usr/lib/domoleaf");
 from DaemonConfigParser import *;
-from MysqlHandler import *;
 
 MASTER_CONF_FILE_FROM            = '/etc/domoleaf/master.conf.save';
 MASTER_CONF_FILE_TO              = '/etc/domoleaf/master.conf';
-SLAVE_CONF_FILE                  = '/etc/domoleaf/master.conf';
+SLAVE_CONF_FILE                  = '/etc/domoleaf/slave.conf';
 
 def master_conf_copy():
     file_from = DaemonConfigParser(MASTER_CONF_FILE_FROM);
@@ -47,10 +47,8 @@ def master_conf_initdb():
     #mysql password
     password = ''.join(random.choice(string.ascii_uppercase + string.ascii_lowercase + string.digits) for _ in range(128))
     password = sha1(password.encode('utf-8'))
-    e9d487501675015782e50fa134b86a903d763a8a
     file.writeValueFromSection('mysql', 'password', password.hexdigest());
     os.system('sed -i "s/define(\'DB_PASSWORD\', \'domoleaf\')/define(\'DB_PASSWORD\', \''+password.hexdigest()+'\')/g" /etc/domoleaf/www/config.php')
-    file_to.writeValueFromSection('mysql', 'password', password.hexdigest());
     
     #mysql user
     query1 = 'DELETE FROM user WHERE User="domoleaf"';
@@ -59,11 +57,16 @@ def master_conf_initdb():
     query4 = "INSERT INTO db (\`Host\`, \`Db\`, \`User\`, \`Select_priv\`, \`Insert_priv\`, \`Update_priv\`, \`Delete_priv\`, \`Create_priv\`, \`Drop_priv\`, \`Grant_priv\`, \`References_priv\`, \`Index_priv\`, \`Alter_priv\`, \`Create_tmp_table_priv\`, \`Lock_tables_priv\`, \`Create_view_priv\`, \`Show_view_priv\`, \`Create_routine_priv\`, \`Alter_routine_priv\`, \`Execute_priv\`, \`Event_priv\`, \`Trigger_priv\`) VALUES ('localhost','domoleaf','domoleaf','Y','Y','Y','Y','Y','Y','Y','Y','Y','Y','Y','Y','Y','Y','Y','Y','Y','Y','Y');";
     query5 = 'FLUSH PRIVILEGES';
     
-    self.sql.mysql_handler_personnal_query(query1);
-    self.sql.mysql_handler_personnal_query(query2);
-    self.sql.mysql_handler_personnal_query(query3);
-    self.sql.mysql_handler_personnal_query(query4);
-    self.sql.mysql_handler_personnal_query(query5);
+    Popen(['mysql', '--defaults-file=/etc/mysql/debian.cnf', 'domoleaf',
+           '-e', query1], stdin=PIPE, stdout=PIPE, stderr=PIPE, bufsize=-1);
+    Popen(['mysql', '--defaults-file=/etc/mysql/debian.cnf', 'domoleaf',
+           '-e', query2], stdin=PIPE, stdout=PIPE, stderr=PIPE, bufsize=-1);
+    Popen(['mysql', '--defaults-file=/etc/mysql/debian.cnf', 'domoleaf',
+           '-e', query3], stdin=PIPE, stdout=PIPE, stderr=PIPE, bufsize=-1);
+    Popen(['mysql', '--defaults-file=/etc/mysql/debian.cnf', 'domoleaf',
+           '-e', query4], stdin=PIPE, stdout=PIPE, stderr=PIPE, bufsize=-1);
+    Popen(['mysql', '--defaults-file=/etc/mysql/debian.cnf', 'domoleaf',
+           '-e', query5], stdin=PIPE, stdout=PIPE, stderr=PIPE, bufsize=-1);
     
 def master_conf_init():
     file = DaemonConfigParser(SLAVE_CONF_FILE);
@@ -82,10 +85,12 @@ def master_conf_init():
     glslave = fic.readline();
     fic.close()
     
-    query1 = "INSERT INTO daemon (name, serial, secretkey, validation, version) VALUES ('"+hostname+"','"+hostname+"','"+personnal_key+"',1,'"+glslave+"')"
-    query1 = "INSERT INTO daemon_protocol (daemon_id, protocol_id, interface) VALUES (1,1,'"+knx+"')"
-    self.sql.mysql_handler_personnal_query(query1);
-    self.sql.mysql_handler_personnal_query(query2);
+    query1 = "INSERT INTO daemon (name, serial, secretkey, validation, version) VALUES ('"+hostname+"','"+hostname+"','"+personnal_key+"',1,'"+glslave.split('\n')[0]+"')"
+    query2 = "INSERT INTO daemon_protocol (daemon_id, protocol_id, interface) VALUES (1,1,'"+knx+"')"
+    Popen(['mysql', '--defaults-file=/etc/mysql/debian.cnf', 'domoleaf',
+           '-e', query1], stdin=PIPE, stdout=PIPE, stderr=PIPE, bufsize=-1);
+    Popen(['mysql', '--defaults-file=/etc/mysql/debian.cnf', 'domoleaf',
+           '-e', query2], stdin=PIPE, stdout=PIPE, stderr=PIPE, bufsize=-1);
 
 if __name__ == "__main__":
     #Upgrade
