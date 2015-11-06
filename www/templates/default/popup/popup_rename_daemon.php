273f906e7cae976f58ae8b000487499c5db2fb90
hub_cloud <giorda_a@epitech.eu>
2015-11-06 09:56:24 +0100

    fix interface protocol and add 'interface_arg'

--- www/templates/default/popup/popup_rename_daemon.php
+++ www/templates/default/popup/popup_rename_daemon.php
@@ -57,19 +57,24 @@ echo	'
 
 echo '<div id="div-interface-KNXTP">'.
 		'<label class="control-label" for="label-interface-KNXTP">'._('KNX TP Interface').'</label>'.
-		'<select id="select-interface-KNXTP" class="selectpicker form-control" onchange="CheckKNXTPIP()">'.
-			'<option value="ttyAMA0">'._('Serial').'</option>'.
-			'<option value="ttyS0">'._('Serial 0').'</option>'.
-			'<option value="ttyS1">'._('Serial 1').'</option>'.
-			'<option value="ttyS2">'._('Serial 2').'</option>'.
-			'<option value="IP">'._('IP').'</option>'.
+		'<select id="select-interface-KNXTP" class="selectpicker form-control" title="KNX TP" onchange="CheckKNXTPType()">'.
+			'<option value="tpuarts">'._('Serial Interface').'</option>'.
+			'<option value="ipt">'._('IP Interface').'</option>'.
 		'</select>'.
-		'<div id="div-interface-KNXTP-IP" class="input-group">'.
+		'<div id="div-interface-KNXTP-Serial" class="margin-top-4">'.
+			'<select id="select-interface-KNXTP-Serial" class="selectpicker form-control" onchange="">'.
+				'<option value="ttyAMA0">'._('Serial').'</option>'.
+				'<option value="ttyS0">'._('Serial 0').'</option>'.
+				'<option value="ttyS1">'._('Serial 1').'</option>'.
+				'<option value="ttyS2">'._('Serial 2').'</option>'.
+			'</select>'.
+		'</div>'.
+		'<div id="div-interface-KNXTP-IP" class="margin-top-4 input-group">'.
 			'<label class="input-group-addon" for="label-interface-KNXTP-IP">'.
 				'<span class="glyphicon glyphicon-user" aria-hidden="true"></span>'.
 			'</label>';
-		if (!empty($daemon->protocol->{1}->interface) && filter_var($daemon->protocol->{1}->interface, FILTER_VALIDATE_IP)){
-			echo '<input type="text" id="input-interface-KNXTP-IP" placeholder="'._('Enter IP').'" value="'.$daemon->protocol->{1}->interface.'" class="form-control">';
+		if (!empty($daemon->protocol->{1}->interface) && filter_var($daemon->protocol->{1}->interface_arg, FILTER_VALIDATE_IP)){
+			echo '<input type="text" id="input-interface-KNXTP-IP" placeholder="'._('Enter IP').'" value="'.$daemon->protocol->{1}->interface_arg.'" class="form-control">';
 		}
 		else{
 			echo '<input type="text" id="input-interface-KNXTP-IP" placeholder="'._('Enter IP').'" value="" class="form-control">';
@@ -80,20 +85,31 @@ echo 	'</div>'.
 	 '<br/>'.
 	 '<div id="div-interface-EnOcean">'.
 	 '<label class="control-label" for="label-interface-EnOcean">'._('EnOcean Interface').'</label>'.
-		'<select id="select-interface-EnOcean" class="selectpicker form-control" title="KNX TP" onchange="CheckEnOceanIP()">'.
-			'<option value="ttyUSB0">'._('USB').'</option>'.
-			'<option value="ttyAMA0">'._('Serial').'</option>'.
-			'<option value="ttyS0">'._('Serial 0').'</option>'.
-			'<option value="ttyS1">'._('Serial 1').'</option>'.
-			'<option value="ttyS2">'._('Serial 2').'</option>'.
-			'<option value="IP">'._('IP').'</option>'.
+		'<select id="select-interface-EnOcean" class="selectpicker form-control" title="EnOcean" onchange="CheckEnOceanType()">'.
+			'<option value="usb">'._('USB Interface').'</option>'.
+			'<option value="tpuarts">'._('Serial Interface').'</option>'.
+			'<option value="ipt">'._('IP Interface').'</option>'.
 		'</select>'.
-		'<div id="div-interface-EnOcean-IP" class="input-group">'.
+		'<div id="div-interface-EnOcean-usb" class="margin-top-4">'.
+			'<select id="select-interface-EnOcean-usb" class="selectpicker form-control" onchange="">'.
+				'<option value="ttyUSB0">'._('USB 0').'</option>'.
+				'<option value="ttyUSB1">'._('USB 1').'</option>'.
+			'</select>'.
+		'</div>'.
+		'<div id="div-interface-EnOcean-Serial" class="margin-top-4">'.
+			'<select id="select-interface-EnOcean-Serial" class="selectpicker form-control" onchange="">'.
+				'<option value="ttyAMA0">'._('Serial').'</option>'.
+				'<option value="ttyS0">'._('Serial 0').'</option>'.
+				'<option value="ttyS1">'._('Serial 1').'</option>'.
+				'<option value="ttyS2">'._('Serial 2').'</option>'.
+			'</select>'.
+		'</div>'.
+		'<div id="div-interface-EnOcean-IP" class="margin-top-4 input-group">'.
 			'<label class="input-group-addon" for="label-interface-EnOcean-IP">'.
 				'<span class="glyphicon glyphicon-user" aria-hidden="true"></span>'.
 			'</label>';
-		if (!empty($daemon->protocol->{2}->interface) && filter_var($daemon->protocol->{2}->interface, FILTER_VALIDATE_IP)){
-			echo '<input type="text" id="input-interface-EnOcean-IP" placeholder="'._('Enter IP').'" value="'.$daemon->protocol->{2}->interface.'" class="form-control">';
+		if (!empty($daemon->protocol->{2}->interface) && filter_var($daemon->protocol->{2}->interface_arg, FILTER_VALIDATE_IP)){
+			echo '<input type="text" id="input-interface-EnOcean-IP" placeholder="'._('Enter IP').'" value="'.$daemon->protocol->{2}->interface_arg.'" class="form-control">';
 		}
 		else{
 			echo '<input type="text" id="input-interface-EnOcean-IP" placeholder="'._('Enter IP').'" value="" class="form-control">';
@@ -103,35 +119,45 @@ echo	'</div>'.
 	 '</div>'.
 	 '<script type="text/javascript">'.
 	 	'$("#select-interface-KNXTP").selectpicker();'.
-	 	'$("#select-interface-EnOcean").selectpicker();';
+	 	'$("#select-interface-KNXTP-Serial").selectpicker();'.
+	 	'$("#select-interface-EnOcean").selectpicker();'.
+	 	'$("#select-interface-EnOcean-usb").selectpicker();'.
+		'$("#select-interface-EnOcean-Serial").selectpicker();';
 	
 	if (!empty($daemon->protocol->{1}->interface)){
-		if (!(filter_var($daemon->protocol->{1}->interface, FILTER_VALIDATE_IP))){
-			echo '$("#select-interface-KNXTP").selectpicker(\'val\', \''.$daemon->protocol->{1}->interface.'\');';
+		if ($daemon->protocol->{1}->interface == 'tpuarts'){
+			echo '$("#select-interface-KNXTP").selectpicker(\'val\', \'tpuarts\');';
+			echo '$("#select-interface-KNXTP-Serial").selectpicker(\'val\', \''.$daemon->protocol->{1}->interface_arg.'\');';
 		}
-		else{
-			echo '$("#select-interface-KNXTP").selectpicker(\'val\', \'IP\');';
+		else if (filter_var($daemon->protocol->{1}->interface_arg, FILTER_VALIDATE_IP)){
+			echo '$("#select-interface-KNXTP").selectpicker(\'val\', \'ipt\');';
 		}
 	}
 	else{
-		echo '$("#select-interface-KNXTP").selectpicker(\'val\', \'ttyAMA0\');';
+		echo '$("#select-interface-KNXTP").selectpicker(\'val\', \'\');';
 	}
 	
 	if (!empty($daemon->protocol->{2}->interface)){
-		if (!(filter_var($daemon->protocol->{2}->interface, FILTER_VALIDATE_IP))){
-			echo '$("#select-interface-EnOcean").selectpicker(\'val\', \''.$daemon->protocol->{2}->interface.'\');';
+		if ($daemon->protocol->{2}->interface == 'usb'){
+			echo '$("#select-interface-EnOcean").selectpicker(\'val\', \'usb\');';
+			echo '$("#select-interface-EnOcean-usb").selectpicker(\'val\', \''.$daemon->protocol->{2}->interface_arg.'\');';
 		}
-		else{
-			echo '$("#select-interface-EnOcean").selectpicker(\'val\', \'IP\');';
+		else if ($daemon->protocol->{2}->interface == 'tpuarts'){
+			echo '$("#select-interface-EnOcean").selectpicker(\'val\', \'tpuarts\');';
+			echo '$("#select-interface-EnOcean-Serial").selectpicker(\'val\', \''.$daemon->protocol->{2}->interface_arg.'\');';
+		}
+		else if (filter_var($daemon->protocol->{2}->interface_arg, FILTER_VALIDATE_IP)){
+			echo '$("#select-interface-EnOcean").selectpicker(\'val\', \''.$daemon->protocol->{2}->interface_arg.'\');';
+			echo '$("#select-interface-EnOcean").selectpicker(\'val\', \'ipt\');';
 		}
 	}
 	else{
-		echo '$("#select-interface-EnOcean").selectpicker(\'val\', \'ttyUSB0\');';
+		echo '$("#select-interface-EnOcean").selectpicker(\'val\', \'\');';
 	}
 	
-	echo 'CheckKNXTPIP();'.
+	echo 'CheckKNXTPType();'.
 		 'CheckKNXTP();'.
-		 'CheckEnOceanIP();'.
+		 'CheckEnOceanType();'.
 		 'CheckEnOcean();'.
 	 '</script>';
 
