673c2901ceb889fbd748f4aefa640f14c6472480
V-Paranoiaque <vr@greenleaf.fr>
2015-11-06 11:39:32 +0100

    SQL for room and floor bg image

--- glmaster/etc/domoleaf/sql/create/user_floor.sql
+++ glmaster/etc/domoleaf/sql/create/user_floor.sql
@@ -1,8 +1,9 @@
-CREATE TABLE IF NOT EXISTS `user_floor` (
+CREATE TABLE `user_floor` (
   `user_id` int(10) unsigned NOT NULL,
   `floor_id` int(10) unsigned NOT NULL,
   `floor_allowed` tinyint(3) unsigned NOT NULL DEFAULT '0',
   `floor_order` int(10) unsigned NOT NULL DEFAULT '0',
+  `floor_bgimg` varchar(255) DEFAULT NULL,
   KEY `user_id` (`user_id`),
   KEY `floor_id` (`floor_id`),
   CONSTRAINT `user_floor_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE,
--- glmaster/etc/domoleaf/sql/create/user_room.sql
+++ glmaster/etc/domoleaf/sql/create/user_room.sql
@@ -1,8 +1,9 @@
-CREATE TABLE IF NOT EXISTS `user_room` (
+CREATE TABLE `user_room` (
   `user_id` int(10) unsigned NOT NULL,
   `room_id` int(10) unsigned NOT NULL,
   `room_allowed` tinyint(3) unsigned NOT NULL DEFAULT '0',
   `room_order` int(10) unsigned NOT NULL DEFAULT '0',
+  `room_bgimg` varchar(255) DEFAULT NULL,
   KEY `user_id` (`user_id`),
   KEY `room_id` (`room_id`),
   CONSTRAINT `user_room_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE,
