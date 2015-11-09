24830a59587215c1a045d20ba375d97b65220e0b
V-Paranoiaque <vr@greenleaf.fr>
2015-11-09 13:49:19 +0100

    Fix knx interface installation

--- domomaster/usr/bin/domomaster/domomaster_postinst.py
+++ domomaster/usr/bin/domomaster/domomaster_postinst.py
@@ -70,11 +70,14 @@ def master_conf_init():
     
     #KNX Interface
     if os.path.exists('/dev/ttyAMA0'):
-        knx = 'ttyAMA0';
+        knx = "tpuarts"
+        knx_interface = 'ttyAMA0';
     elif os.path.exists('/dev/ttyS0'):
-        knx = 'ttyS0';
+        knx = "tpuarts"
+        knx_interface = 'ttyS0';
     else:
-        knx = '127.0.0.1';
+        knx = "ipt"
+        knx_interface = '127.0.0.1';
     
     fic = open('/etc/domoleaf/.domoslave.version','r')
     domoslave = fic.readline();
@@ -83,7 +86,7 @@ def master_conf_init():
     personnal_key = md5(personnal_key.encode('utf-8'))
     
     query1 = "INSERT INTO daemon (name, serial, secretkey, validation, version) VALUES ('"+hostname+"','"+hostname+"','"+personnal_key.hexdigest()+"',1,'"+domoslave.split('\n')[0]+"')"
-    query2 = "INSERT INTO daemon_protocol (daemon_id, protocol_id, interface) VALUES (1,1,'"+knx+"')"
+    query2 = "INSERT INTO daemon_protocol (daemon_id, protocol_id, interface, interface_arg) VALUES (1,1,'"+knx+"','"+knx_interface+"')"
     Popen(['mysql', '--defaults-file=/etc/mysql/debian.cnf', 'domoleaf',
            '-e', query1], stdin=PIPE, stdout=PIPE, stderr=PIPE, bufsize=-1);
     Popen(['mysql', '--defaults-file=/etc/mysql/debian.cnf', 'domoleaf',
