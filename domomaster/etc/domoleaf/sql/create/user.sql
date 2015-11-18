1ec3fa522ebf5aaec70b941419ef6f49a2cb8b1e
V-Paranoiaque <vr@greenleaf.fr>
2015-11-18 10:03:19 +0100

    Add SQL for border's color

--- domomaster/etc/domoleaf/sql/create/user.sql
+++ domomaster/etc/domoleaf/sql/create/user.sql
@@ -10,7 +10,8 @@ CREATE TABLE IF NOT EXISTS `user` (
   `phone` varchar(64) DEFAULT '',
   `language` varchar(3) NOT NULL DEFAULT 'fr',
   `design` tinyint(3) unsigned NOT NULL DEFAULT '0',
-  `bg_color` varchar(15) NOT NULL DEFAULT '#eee',
+  `bg_color` varchar(15) DEFAULT '#eee',
+  `border_color` varchar(15) DEFAULT '#f5f5f5',
   `activity` int(10) unsigned DEFAULT '0',
   PRIMARY KEY (`user_id`),
   UNIQUE KEY `user_mail` (`user_mail`)
