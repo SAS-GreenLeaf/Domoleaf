f393aa21bfc15ea12ea441f2ee681ffa4d64f6f7
hub_cloud <giorda_a@epitech.eu>
2015-11-25 16:41:43 +0100

    change order user/backups

--- www/templates/default/include/configuration-menu.php
+++ www/templates/default/include/configuration-menu.php
@@ -6,8 +6,8 @@ echo
 		<a title="'._('General').'" href="/conf_general" id="menu-general" class="list-group-item text-center change">
 			<h4 class="fa fa-cog lg"></h4><br/><span class="hidden-xs">'._('General').'</span>
 		</a>
-		<a title="'._('Backups').'" href="/conf_db" id="menu-database" class="list-group-item text-center change">
-			<h4 class="fa fa-database lg"></h4><br/><span class="hidden-xs">'._('Backups').'</span>
+		<a title="'._('Users').'" href="/conf_users" id="menu-users" class="list-group-item text-center change">
+			<h4 class="fa fa-users lg"></h4><br/><span class="hidden-xs">'._('Users').'</span>
 		</a>
 		<a title="'._('Installation').'" href="/conf_installation" id="menu-install" class="list-group-item text-center change">
 			<h4 class="fa fa-flask lg"></h4><br/><span class="hidden-xs">'._('Installation').'</span>
@@ -15,8 +15,8 @@ echo
 		<a title="'._('Box').'" href="/conf_daemon" id="menu-box" class="list-group-item text-center change">
 			<h4 class="fa fa-cubes lg"></h4><br/><span class="hidden-xs">'._('Box').'</span>
 		</a>
-		<a title="'._('Users').'" href="/conf_users" id="menu-users" class="list-group-item text-center change">
-			<h4 class="fa fa-users lg"></h4><br/><span class="hidden-xs">'._('Users').'</span>
+		<a title="'._('Backups').'" href="/conf_db" id="menu-database" class="list-group-item text-center change">
+			<h4 class="fa fa-database lg"></h4><br/><span class="hidden-xs">'._('Backups').'</span>
 		</a>
 		<a title="'._('Busmonitor').'" href="#" onclick="ShowBusmonitor()" class="list-group-item text-center change">
 			<h4 class="fa fa-wrench lg"></h4><br/><span class="hidden-xs">'._('Busmonitor').'</span>
