e44bc042958254255d4485c5c7abc107422eb243
V-Paranoiaque <vr@greenleaf.fr>
2016-02-16 10:23:39 +0100

    Add area to be able to disable the monitor daemon.

--- domomaster/etc/domoleaf/sql/create/daemon_protocol.sql
+++ domomaster/etc/domoleaf/sql/create/daemon_protocol.sql
@@ -4,6 +4,7 @@ CREATE TABLE IF NOT EXISTS `daemon_protocol` (
   `interface` varchar(63) DEFAULT NULL,
   `interface_arg` varchar(63) DEFAULT NULL,
   `interface_addr` varchar(63) DEFAULT NULL,
+  `daemon_activated` tinyint(3) unsigned NOT NULL DEFAULT '1',
   KEY `daemon_id` (`daemon_id`),
   KEY `protocol_id` (`protocol_id`),
   CONSTRAINT `daemon_protocol_ibfk_1` FOREIGN KEY (`daemon_id`) REFERENCES `daemon` (`daemon_id`) ON DELETE CASCADE ON UPDATE CASCADE,
