bb86d50a6590e1ed5ed841d79872965aa1e4a142
V-Paranoiaque <vr@greenleaf.fr>
2016-02-25 10:45:29 +0100

    new areas for later

--- domomaster/etc/domoleaf/sql/create/room_device_option.sql
+++ domomaster/etc/domoleaf/sql/create/room_device_option.sql
@@ -1,17 +1,20 @@
 CREATE TABLE IF NOT EXISTS `room_device_option` (
   `room_device_id` int(11) unsigned NOT NULL,
   `option_id` int(11) unsigned NOT NULL,
-  `addr` varchar(2047) DEFAULT NULL,
-  `addr_plus` varchar(2047) DEFAULT NULL,
-  `dpt_id` int(10) unsigned DEFAULT NULL,
+  `addr` varchar(255) DEFAULT NULL,
+  `addr_plus` varchar(255) DEFAULT NULL,
+  `dpt_id` int(11) unsigned DEFAULT NULL,
   `status` tinyint(3) unsigned NOT NULL DEFAULT '0',
   `opt_value` varchar(15) NOT NULL DEFAULT '',
+  `opt_label` varchar(255) DEFAULT NULL,
+  `opt_color` varchar(32) DEFAULT NULL,
+  `opt_bgimg` varchar(255) DEFAULT NULL,
   KEY `room_device_id` (`room_device_id`),
   KEY `option_id` (`option_id`),
   KEY `addr_plus` (`addr_plus`),
-  KEY `addr` (`addr`(767)),
+  KEY `addr` (`addr`),
   KEY `dpt_id` (`dpt_id`),
   CONSTRAINT `room_device_option_ibfk_1` FOREIGN KEY (`room_device_id`) REFERENCES `room_device` (`room_device_id`) ON DELETE CASCADE ON UPDATE CASCADE,
   CONSTRAINT `room_device_option_ibfk_2` FOREIGN KEY (`option_id`) REFERENCES `optiondef` (`option_id`) ON DELETE CASCADE ON UPDATE CASCADE,
   CONSTRAINT `room_device_option_ibfk_3` FOREIGN KEY (`dpt_id`) REFERENCES `dpt` (`dpt_id`) ON DELETE SET NULL ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1
