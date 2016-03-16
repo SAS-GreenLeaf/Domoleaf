4a011fd205bde44e85d9cbb6dc2b8adafd3f1896
V-Paranoiaque <vr@greenleaf.fr>
2016-03-16 15:59:46 +0100

    docker scripts for debian/jessie and ubuntu/xenial

---
+++ docker/ubuntu/xenial/build.sh
@@ -0,0 +1,17 @@
+SYS="ubuntu"
+DISTRIB="xenial"
+NAME="domoleaf${SYS}${DISTRIB}"
+CURRENT=${PWD##*/} 
+if [ $CURRENT = $DISTRIB ] ; then
+	cd ../../../
+fi
+cp docker/$SYS/$DISTRIB/dockerfile .
+docker build -t $NAME .
+rm -f dockerfile
+docker run --name $NAME -itd $NAME
+for file in $(docker exec $NAME ls /root/ | grep .deb$) ; do
+	docker cp $NAME:/root/$file .
+done
+sleep 1
+docker stop $NAME
+docker rm $NAME
