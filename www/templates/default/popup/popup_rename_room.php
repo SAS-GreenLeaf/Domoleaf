067262e97d70b32ad4776751646c447271ceeacc
V-Paranoiaque <vr@greenleaf.fr>
2016-06-03 16:52:48 +0200

    Fix php and indentation

--- www/templates/default/popup/popup_rename_room.php
+++ www/templates/default/popup/popup_rename_room.php
@@ -11,22 +11,22 @@ $roomlist = $result->confRoomList;
 $floorlist = $result->confFloorList;
 
 echo '<div class="center">';
-	printf(_('Do you want to rename %s?'), '<strong>'.$roomlist->{$_GET['idroom']->room_name.'</strong>'); 
+	printf(_('Do you want to rename %s?'), '<strong>'.$roomlist->{$_GET['idroom']}->room_name.'</strong>'); 
 echo'</div
 <div class="controls center">	
 	<div class="input-group">
 	<span class="input-group-addon">'._('New name').'</span>'.
-	  '<input type="text" id="newroomname" class="form-control" value="'.$roomlist->{$_GET['idroom']->room_name.'" aria-describedby="basic-addon1">
+	  '<input type="text" id="newroomname" class="form-control" value="'.$roomlist->{$_GET['idroom']}->room_name.'" aria-describedby="basic-addon1">
 	</div>
 	<select class="selectpicker form-control" id="changefloor">';
-	  	foreach($floorlist as $elem){
-	  		if ($elem->floor_name == $roomlist->{$_GET['idroom']}->floor_name){
-	  			echo '<option value="'.$elem->floor_id.'" selected>'.$elem->floor_name.'</option>';
-	  		}
-	  		else {
-	  			echo '<option value="'.$elem->floor_id.'">'.$elem->floor_name.'</option>';
-	  		}
-	  	}	
+		foreach($floorlist as $elem){
+			if ($elem->floor_name == $roomlist->{$_GET['idroom']}->floor_name){
+				echo '<option value="'.$elem->floor_id.'" selected>'.$elem->floor_name.'</option>';
+			}
+			else {
+				echo '<option value="'.$elem->floor_id.'">'.$elem->floor_name.'</option>';
+			}
+		}	
 echo
 	'</select><br/><br/>'.
 	'<button id="eventSave"'.
