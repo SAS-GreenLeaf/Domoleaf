9beb668c19760a720fe8d7ce9df801c955a580ac
V-Paranoiaque <vr@greenleaf.fr>
2016-06-15 15:29:42 +0200

    Fix php

--- www/templates/default/popup/popup_remove_device.php
+++ www/templates/default/popup/popup_remove_device.php
@@ -13,7 +13,7 @@ if (!empty($_GET['iddevice'])){
 	$listdevice = $result->confRoomDeviceAll;
 
 	echo '<div class="center">';
-		printf(_('Do you want to delete %s?'), '<strong>'.$listdevice-{>$_GET['iddevice']}->name.'</strong>');
+		printf(_('Do you want to delete %s?'), '<strong>'.$listdevice->{$_GET['iddevice']}->name.'</strong>');
 	echo '</div><div class="center">
 		<button onclick="RemoveDevice('.$_GET['iddevice'].')" class="btn btn-greenleaf">'._('Yes').' <span class="glyphicon glyphicon-ok"></span></button> <button onclick="popup_close()" class="btn btn-danger">'._('No').' <span class="glyphicon glyphicon-remove"></span></button>
 	</div>';
