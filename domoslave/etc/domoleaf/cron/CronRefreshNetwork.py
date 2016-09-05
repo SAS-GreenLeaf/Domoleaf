438e3442becb8352c42b67363fb55f51a13597ea
Emmanuel Bonin <eb@greenleaf.fr>
2016-09-05 15:42:51 +0200

    Added comments in the slave cron

--- domoslave/etc/domoleaf/cron/CronRefreshNetwork.py
+++ domoslave/etc/domoleaf/cron/CronRefreshNetwork.py
@@ -4,5 +4,8 @@ import sys;
 sys.path.append('/usr/lib/domoleaf');
 from GLManager import *;
 
+"""
+Cron that refreshes the list of connected devices on the local network
+"""
 if __name__ == "__main__":
     GLManager.send_cron('monitor_ip')
--- domoslave/etc/domoleaf/cron/CronSendAlive.py
+++ domoslave/etc/domoleaf/cron/CronSendAlive.py
@@ -4,5 +4,8 @@ import sys;
 sys.path.append('/usr/lib/domoleaf');
 from GLManager import *;
 
+"""
+Cron that sends the information that the slave is alive
+"""
 if __name__ == "__main__":
     GLManager.send_cron('send_alive')
