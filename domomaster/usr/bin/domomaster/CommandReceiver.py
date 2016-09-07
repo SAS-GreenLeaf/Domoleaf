d456b6b50e383348db2d6e6e169f97c7ee6d46e3
Manu <eb@greenleaf.fr>
2016-09-07 10:36:39 +0200

    Updated the comments to match doxygen syntax

--- domomaster/usr/bin/domomaster/CommandReceiver.py
+++ domomaster/usr/bin/domomaster/CommandReceiver.py
@@ -1,12 +1,22 @@
+## @package domomaster
+# Master daemon for D3 boxes.
+#
+# Developed by GreenLeaf.
+
 from threading import Thread;
 from MysqlHandler import *;
 import MasterDaemon;
 
+## Threaded class retrieving a domoleaf packet.
+#
+# Retrieves packets from the slaves, and sends it to treatment function.
 class CommandReceiver(Thread):
+
+    ## The constructor.
+    #
+    # @param connection The connection object used to receive data.
+    # @param daemon The MasterDaemon object.
     def __init__(self, connection, daemon):
-        """
-        Threaded class retrieving a domoleaf packet and sends it to treatment function
-        """
         Thread.__init__(self);
         self.connection = connection;
         self.daemon = daemon;
@@ -14,10 +24,10 @@ class CommandReceiver(Thread):
         self.db_passwd = daemon.db_passwd;
         self.db_dbname = daemon.db_dbname;
 
+    ## Thread run function overload.
+    #
+    # It listens to the slaves and sends the packet to the treatment function.
     def run(self):
-        """
-        Thread run function overload
-        """
         self.db = MysqlHandler(self.db_username, self.db_passwd, self.db_dbname);
         data = self.connection.recv(MasterDaemon.MAX_DATA_LENGTH).decode();
         self.daemon.parse_data(data, self.connection, 0, self.db);
