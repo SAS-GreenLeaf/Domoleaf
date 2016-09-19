4358f17b2a480dab2fda5e941092715cfb91f498
Manu <eb@greenleaf.fr>
2016-09-19 14:37:47 +0200

    Added doxygen docs for monitor_knx

--- monitor_knx/knx_check_args.c
+++ monitor_knx/knx_check_args.c
@@ -1,22 +1,32 @@
+/**
+ * \file knx_check_args.c
+ * \brief Function to check the command line arguments of the program
+ * \author Emmanuel Bonin - Greenleaf
+ */
+
 #include "knx.h"
 
-/*
-** Check program's arguments
-*/
+/**
+ * \fn void check_args(int argc, char *argv[])
+ * \param argc The argument count received
+ * \param argv The argument values received
+ *
+ * \brief Check program's arguments and exits on error
+ */
 void check_args(int argc, char *argv[])
 {
-	if (argc < 2)
-	{
-		fprintf(stderr, "Usage: %s url\n", argv[0]);
-		exit(EXIT_FAILURE);
-	}
-	else if (strncmp(argv[1], "ip:", 3) != 0)
-	{
-		fprintf(stderr, "url param must be of the form ip:xxx.xxx.xxx.xxx\n");
-		exit(EXIT_FAILURE);
-	}
-	else if (argc == 4)
-	{
-		sleep(atoi(argv[3]));
-	}
+  if (argc < 2)
+    {
+      fprintf(stderr, "Usage: %s url\n", argv[0]);
+      exit(EXIT_FAILURE);
+    }
+  else if (strncmp(argv[1], "ip:", 3) != 0)
+    {
+      fprintf(stderr, "url param must be of the form ip:xxx.xxx.xxx.xxx\n");
+      exit(EXIT_FAILURE);
+    }
+  else if (argc == 4)
+    {
+      sleep(atoi(argv[3]));
+    }
 }
