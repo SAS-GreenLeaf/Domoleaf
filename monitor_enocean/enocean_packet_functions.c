5213d83db2b7a983f4b806efad37bd5400941dbe
Emmanuel Bonin <eb@greenleaf.fr>
2016-11-09 17:00:50 +0100

    Fixed the bug pushed by coutur_g which was causing the monitor to segfault

--- monitor_enocean/enocean_packet_functions.c
+++ monitor_enocean/enocean_packet_functions.c
@@ -295,8 +295,9 @@ Enocean_packet create_packet(uint8_t *buffer, uint16_t data_len, uint8_t opt_dat
   Enocean_packet res;
 
   memset(&res, 0, sizeof(res));
+  res.data = malloc(255);
   res.sync_byte = buffer[0];
-  res.header.data_length = data_len - 6;
+  res.header.data_length = data_len;
   res.header.opt_data_length = opt_data_len;
   res.header.packet_type = packet_type;
   res.CRC8H = buffer[5];
